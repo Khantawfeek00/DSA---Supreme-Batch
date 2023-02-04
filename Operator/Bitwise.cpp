70fd908216f946aed102391426ae3a9ff23e3da0
Tawfeek Khan <khantawfeek00@gmail.com>
2023-02-04 16:06:27 +0530

    Operator Folder added

---
+++ Operator/Bitwise.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    bool a = false;
+    bool b = false;
+
+    cout << (a & b) << endl;
+    cout << (a | b) << endl;
+    cout << (~a) << endl;
+    cout << (a ^ b) << endl;
+
+}
