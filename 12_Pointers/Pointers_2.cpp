482906c86556da7c6c859da88212af79c596c4a0
Tawfeek Khan <khantawfeek00@gmail.com>
2023-07-06 13:12:58 +0530

    Week 4 day 2 complete

--- 12_Pointers/Pointers_2.cpp
+++ 12_Pointers/Pointers_2.cpp
@@ -9,7 +9,7 @@ int solve(int *arr)
 
 int main()
 {
-    // int a[] = {1, 2, 3, 4, 5, 6, 7};
+    int a[] = {1, 2, 3, 4, 5, 6, 7};
     // cout << a << endl;
     // cout << &a << endl;
     // cout << &a[0] << endl;
@@ -19,39 +19,60 @@ int main()
     // cout << p << endl;
     // cout << &p << endl;
 
-    // int *p = &a;
+    // error
+    //  int *p = &a;
+
+    // int *p = a;
     // cout << 3 [a] << endl;
     // cout << *a << endl;
     // cout << *a + 1 << endl;
 
-    // cout << a + 0 << endl;
-    // cout << a + 1 << endl;6
-    // cout << *(a + 1) + 2 << endl;
+    // cout<< a + 0 << endl;
+    // cout<< a + 1 << endl;6
+    // cout<< *(a + 1) + 2 << endl;
 
-    // cout << a[3] << endl;  // *(a+3);
-    // cout << 3 [a] << endl; //*(3+a);
+    // cout<< a[3] << endl;  // *(a+3);
+    // cout<< 3 [a] << endl; //*(3+a);
 
     // int *p = a;
-    // cout << a << endl;
-    // cout << &p << endl;
+    // cout<< a << endl;
+    // cout<< &p << endl;
     // // p = p + 1;
-    // cout << p << endl;
+    // cout<< p << endl;
 
     // char arr[10] = {'1', '2', '3', '4'};
     // // int *p = arr;
-    // // cout << sizeof(arr) << endl;
-    // // cout << sizeof(p) << endl;
+    // // cout<< sizeof(arr) << endl;
+    // // cout<< sizeof(p) << endl;
     // char *p = &arr[0];
     // char *q = arr;
-    // cout << arr << endl;
-    // cout << p + 2 << endl;
-    // cout << q << endl;
+    // cout<< arr << endl;
+    // cout<< p + 2 << endl;
+    // cout<< q << endl;
 
     // char ch = 'k';
     // char *ptr = &ch;
     // cout << ptr << endl;
 
-    int arr[10] = {11, 2, 3, 4};
-    cout << "Main = " << sizeof(arr) << endl;
-    solve(arr);
+    // int arr[10] = {11, 2, 3, 4};
+    // cout << "Main = " << sizeof(arr) << endl;
+    // solve(arr);
+
+    // int arr[] = {1, 2, 3, 4, 5, 6};
+    // for (int i = 0; i < 6; i++)
+    //     cout<< &arr[i] << " ";
+
+    // int arr[] = {1, 2, 3};
+    // char ch[] = "Hello";
+
+    // cout << arr << endl;
+    // cout << ch << endl;
+
+    // int *ptr = arr;
+    // char *ptr1 = ch;
+
+    // cout << ptr << endl;
+    // cout << ptr1 << endl;
+
+    // cout << &ch[0] << endl;
 }
