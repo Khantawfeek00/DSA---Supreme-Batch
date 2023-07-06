6cffc76736198e1e82fee09ffd47cbecc28fe5e7
Tawfeek Khan <khantawfeek00@gmail.com>
2023-07-06 11:44:04 +0530

    Pointer week 1

--- 12_Pointers/Pointers_1.cpp
+++ 12_Pointers/Pointers_1.cpp
@@ -27,9 +27,47 @@ int main()
     // cout << p << endl;
     // cout << &p << endl;
 
-    char a = '5';
-    char *p = &a;
-    cout << sizeof(*p); // gives sizeof value stored in that address (Complicated)
-    cout << sizeof(&p); // archetecture depenedent //give size of address
-    cout << sizeof(p);  // gives size of address
+    // char a = '5';
+    // char *p = &a;
+    // cout << sizeof(*p); // gives sizeof value stored in that address (Complicated)
+    // cout << sizeof(&p); // archetecture depenedent //give size of address
+    // cout << sizeof(p);  // gives size of address
+
+    // Copying the pointer
+    int a = 5;
+    int *p = &a;
+    int *q = p;
+    cout << "ptr = " << p << endl;
+    cout << "dusraptr = " << q << endl;
+
+    cout << a << endl;
+    cout << &a << endl;
+    cout << p << endl;
+    cout << &p << endl;
+    cout << q << endl;
+    cout << &q << endl;
+    cout << *p << endl;
+    cout << *q << endl;
+    cout << (*p + *q) << endl;
+    cout << (*p) * 2 + (*q) * 3 << endl;
+    cout << (*p / 2) - (*q) / 2 << endl;
+
+    // Revise
+    //  int a = 5, b = 5;
+    //  cout << a << endl;
+    //  cout << &a << endl;
+    //  cout << &b << endl;
+
+    // int a = 5;
+    // cout << a << endl;
+    // int *ptr = &a;
+
+    // value of ptr
+    // cout << ptr << endl;
+
+    // value at address stored in ptr
+    // cout << *ptr << endl;
+
+    // address of ptr
+    // cout << &ptr << endl;
 }
