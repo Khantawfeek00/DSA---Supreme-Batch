ddd4f83979c83799de6d5b5a84b409d71ab37374
Tawfeek Khan <khantawfeek00@gmail.com>
2023-08-22 02:32:10 +0530

    Update First-Non-Repeating-Character-In-Stream.cpp

--- 19_Queue/Assignment/First-Non-Repeating-Character-In-Stream.cpp
+++ 19_Queue/Assignment/First-Non-Repeating-Character-In-Stream.cpp
@@ -3,7 +3,7 @@
 #include <queue>
 #include <deque>
 using namespace std;
-string FirstNonRepeating(string A)
+/*string FirstNonRepeating(string A)
 {
     int arr[26] = {0};
     deque<char> dq;
@@ -27,6 +27,35 @@ string FirstNonRepeating(string A)
         else
         {
             ans.push_back(dq.front());
+        }
+    }
+    return ans;
+}
+
+*/
+
+// T.C.-O(n) S.C.=O(n)
+string FirstNonRepeating(string A)
+{
+    queue<char> que;
+    char arr[26] = {0};
+    string ans;
+    for (char ch : A)
+    {
+        que.push(ch);
+        arr[(int)ch - 'a']++;
+
+        while (!que.empty() && arr[(int)que.front() - 'a'] > 1)
+        {
+            que.pop();
+        }
+        if (que.empty())
+        {
+            ans.push_back('#');
+        }
+        else
+        {
+            ans.push_back(que.front());
         }
     }
     return ans;
